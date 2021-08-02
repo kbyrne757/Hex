1fe9689022021d0bc8931f6051a542f445b904c2
Lemon Zest <k.byrne357@gmail.com>
2021-08-02 17:32:01 +0100

    Hex decoding

--- Hex_encoding.py
+++ Hex_encoding.py
@@ -5,5 +5,9 @@
 userInput = input("Enter string to convert to hex: ")
 userInput = userInput.encode('utf-8')
 hexOutput = userInput.hex()
-
 print (hexOutput)
+
+userInput2 = input("Enter string to convert from hex: ")
+byte_array = bytearray.fromhex(userInput2)
+hexDecode= byte_array.decode()
+print (hexDecode)
