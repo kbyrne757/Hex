5d345c43f26bc4eb659fc6c23a4199ab003c6bfb
Lemon Zest <k.byrne357@gmail.com>
2021-08-02 17:42:27 +0100

    added functions

--- Hex_encoding.py
+++ Hex_encoding.py
@@ -1,13 +1,33 @@
 #python version 3.9.5
 #Hex Encoding/Decoding
 
+def intro():
+    decision = input("Would you like to encode or decode Base64? (encode/decode) ")
+    decision = decision.upper()
 
-userInput = input("Enter string to convert to hex: ")
-userInput = userInput.encode('utf-8')
-hexOutput = userInput.hex()
-print (hexOutput)
+    if (decision == 'ENCODE'):
+        encoding()
 
-userInput2 = input("Enter string to convert from hex: ")
-byte_array = bytearray.fromhex(userInput2)
-hexDecode= byte_array.decode()
-print (hexDecode)
+    
+    elif (decision == 'DECODE'):
+        decoding()
+
+    else:
+        print("Invalid Input")
+
+def encoding():
+        #hex encoding
+        userInput = input("Enter string to convert to hex: ")
+        userInput = userInput.encode('utf-8')
+        hexOutput = userInput.hex()
+        print (hexOutput)
+
+def decoding():
+        #hex decoding
+        userInput2 = input("Enter string to convert from hex: ")
+        byte_array = bytearray.fromhex(userInput2)
+        hexDecode= byte_array.decode()
+        print (hexDecode)
+
+
+intro()
